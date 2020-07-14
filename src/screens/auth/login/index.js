d789fba025023f6a01262e3b095c06c98d7bd527
james-lin71 <lin038532@gmail.com>
2020-07-14 22:36:59 +0200

    Login screen

--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Login Ekran</Text>
+        <Text style={styles.appName}>Black Kardd</Text>
+        <TextInput
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+          placeholder="Email"
+          placeholderTextColor="white"
+          style={styles.textInput} />
+        <TextInput
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
+          placeholder="Password"
+          placeholderTextColor="white"
+          style={styles.textInput} />
+        <View style={styles.forgotPasswordContainer}>
+          <Text style={styles.forgotPasswordText}>Forgot your password?</Text>
+        </View>
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>LOGIN</Text>
+        </TouchableOpacity>
       </View>
     );
   }
--- src/screens/auth/login/styles.js
+++ src/screens/auth/login/styles.js
@@ -1,10 +1,48 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 //import config from 'config';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    backgroundColor: '#000000',
+    alignItems: "center"
   },
+  appName: {
+    color: "#ffffff",
+    fontSize: 30
+  },
+  textInput: {
+    backgroundColor: "#1c1c1e",
+    width: "100%",
+    marginTop: 8,
+    paddingVertical: 19,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+    color: "white",
+    fontSize: 17
+  },
+  forgotPasswordContainer: {
+    alignSelf: "flex-end",
+    marginTop: 18
+  },
+  forgotPasswordText: {
+    color: "#ffffff",
+    opacity: 0.3,
+    fontSize: 14
+  },
+  loginButton: {
+    width: "100%",
+    marginTop: 48,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 14,
+    backgroundColor: "red",
+    borderRadius: 25
+  },
+  loginButtonText: {
+    color: "white",
+    fontWeight: "600"
+  }
 });
 
 export default styles;
